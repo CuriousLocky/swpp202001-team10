0a1643d989eb856cad36ba51907100908e4ba9e6
alphy1 <42415340+alphy1@users.noreply.github.com>
2020-05-17 13:00:43 +0300

    Update malloc-to-alloca

--- src/malloc-to-alloca.cpp
+++ src/malloc-to-alloca.cpp
@@ -1,3 +1,5 @@
+#include "SimpleBackend.h"
+
 #include "llvm/IR/PassManager.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
@@ -5,15 +7,20 @@
 #include "llvm/IR/PatternMatch.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/Function.h"
+#include "llvm/Support/SourceMgr.h"
+#include "llvm/AsmParser/Parser.h"
+#include "llvm/IR/InstIterator.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Module.h"
 #include <vector>
 
 using namespace llvm;
 using namespace std;
 using namespace llvm::PatternMatch;
 
-namespace {
-class MallocToAlloca : public PassInfoMixin<MallocToAlloca> {
-  public: PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
+PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
+  vector <Instruction *> inst_to_change;
+  vector <Instruction *> inst_to_remove;
   // Going through all instructions to find all malloc's that have been freed before function end  
     for (auto &BB : F)  
       for (auto &I : BB)
@@ -26,41 +33,31 @@ class MallocToAlloca : public PassInfoMixin<MallocToAlloca> {
               if (Instruction *UsrI = dyn_cast<Instruction>(Usr)) 
                 if(CallInst *CI = dyn_cast<CallInst>(UsrI)) 
                   if (CI->getCalledFunction()->getName() == "free") {           // current inst is free -> malloc is freed
-                    Function *m = cast <Function> (I.getOperand(1));
-                    Type *t;
-                    for (auto &Arg : m -> args()) {
-                      t = Arg.getType();                                        // to know type of malloc variable
-                    }
-
-                    unsigned int *add = (unsigned int *) malloc(sizeof(unsigned int));
-                    AllocaInst* alloc = new AllocaInst(t, *add, I.getName(), &BB);
-                    I.replaceAllUsesWith(alloc);
-                    UsrI -> eraseFromParent();
+                    inst_to_change.push_back(&I);
+                    inst_to_remove.push_back(&UsrI);
                   }  
             }
-
+    // Changing malloc to alloca
+      for (auto *I: inst_to_change) {
+        
+        Function *m = cast <Function> (I -> getOperand(1));
+        Type *t;
+        for (auto &Arg : m -> args()) {
+          t = Arg.getType();                                        // to know type of malloc variable
+        }
+        outs() << I -> getName() << '\n';
+      /*
+        unsigned int *add = (unsigned int *) malloc(sizeof(unsigned int));
+        AllocaInst* alloc = new AllocaInst(t, *add, I->getName(), &BB);
+      */
+        BasicBlock *parent = I -> getParent();
+        IRBuilder<> ParentBuilder(parent);
+        auto *alloc = ParentBuilder.CreateAlloca(t, nullptr, I->getName());  
+        I -> replaceAllUsesWith(alloc);
+      }
+    // Remove free instructions
+      for (auto *I: inst_to_remove) {
+        I -> eraseFromParent();
+      }
     return PreservedAnalyses::all();
   }
-};
-}
-
-extern "C" ::llvm::PassPluginLibraryInfo
-llvmGetPassPluginInfo() {
-  return {
-    LLVM_PLUGIN_API_VERSION, "MallocToAlloca", "v0.1",
-    [](PassBuilder &PB) {
-      PB.registerPipelineParsingCallback(
-        [](StringRef Name, FunctionPassManager &FPM,
-           ArrayRef<PassBuilder::PipelineElement>) {
-          if (Name == "malloc-to-alloca") {
-            FPM.addPass(MallocToAlloca());
-            return true;
-          }
-          return false;
-        }
-      );
-    }
-  };
-}
-
-
