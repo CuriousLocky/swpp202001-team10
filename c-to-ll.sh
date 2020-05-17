32ebe826bc444f01e46fd12465d6db41f01448d5
CuriousLocky <835322750jm37@outlook.com>
2020-05-17 16:49:44 +0900

    Squashed 'test/' content from commit d14e5f5
    git-subtree-dir: test
    git-subtree-split: d14e5f52d9f814184bac70fb9d48517a2ee5b737

---
+++ c-to-ll.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+  echo "c-to-ll.sh <.c file> <clang dir>"
+  echo "ex)  ./c-to-ll.sh ./bubble_sort/src/bubble_sort.c llvm-10.0-releaseassert/bin"
+  exit 1
+fi
+
+if [[ ! -f irgen ]]; then
+  echo "irgen is not built! please run ./build.sh <clang dir>"
+  exit 1
+fi
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  ISYSROOT="-isysroot `xcrun --show-sdk-path`"
+else
+  ISYSROOT=
+fi
+
+CXX=$2/clang
+
+$CXX $ISYSROOT -O1 -fno-strict-aliasing -fno-discard-value-names -g0 $1 \
+    -mllvm -disable-llvm-optzns -S -o /tmp/a.ll -emit-llvm
+./irgen /tmp/a.ll "${1%.c}.ll"
