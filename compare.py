52076436e3b2fc9e2392d6ad661882a4e2a45514
matchy233 <matchy233@gmail.com>
2020-05-29 19:57:42 +0900

    Add avg improvement calc feature (Resolve #10)

--- compare.py
+++ compare.py
@@ -18,36 +18,49 @@ c = contents[0]
 for line in c:
     if line.startswith('#'):
         test = re.search("^[\#]{4}\s(.*)\s[\#]{4}$", line).group(1)
-        print(test)
         data[test] = {}
+    if line.startswith('='):
+        curr_input = re.search("^={2}\s(.*)\s={2}$", line).group(1)
+        data[test][curr_input] = {}
     if line.startswith('Cost: '):
         cost = float(line[6:])
-        data[test]['cost'] = cost
+        data[test][curr_input]['cost'] = cost
     if line.startswith('Max heap usage (bytes): '):
         heap = float(line[len('Max heap usage (bytes): '):])
-        data[test]['heapUsage'] = heap
+        data[test][curr_input]['heapUsage'] = heap
 
 c = contents[1]
 for line in c:
     if line.startswith('#'):
         test = re.search("^[\#]{4}\s(.*)\s[\#]{4}$", line).group(1)
+    if line.startswith('='):
+        curr_input = re.search("^={2}\s(.*)\s={2}$", line).group(1)
     if line.startswith('Cost: '):
         cost = float(line[6:])
-        old_cost = data[test]['cost']
-        try:
-            data[test]['cost'] = (old_cost - cost)/old_cost
-        except:
-            data[test]['cost'] = "N/A"
+        old_cost = data[test][curr_input]['cost']
+        data[test][curr_input]['cost'] = (old_cost - cost)/old_cost
     if line.startswith('Max heap usage (bytes): '):
         heap = float(line[len('Max heap usage (bytes): '):])
-        old_heap = data[test]['heapUsage'] 
-        try:
-            data[test]['heapUsage'] = (old_heap - heap)/old_heap
-        except:
-            data[test]['heapUsage'] = "N/A"
+        old_heap = data[test][curr_input]['heapUsage'] 
+        if old_heap - heap == 0:
+            data[test][curr_input]['heapUsage'] = 0.0
+        else:
+            data[test][curr_input]['heapUsage'] = (old_heap - heap)/old_heap
+
 
 with open('improve-report.log', 'w') as f:
     for t in data:
-       f.write(f"=={t}==\n")
-       for k in data[t]:
-           f.write(f'{k} improve %: {data[t][k]}\n')
+        f.write(f"=={t}==\n")
+        perc = {}
+        total = 0
+        for i in data[t]:
+            total += 1
+            f.write(f'--{i}--\n')
+            for k in data[t][i]:
+                temp = perc.get(k, 0.0)
+                perc[k] = temp + data[t][i][k]
+                f.write(f'{k} improve %: {data[t][i][k]:.5f}%\n')
+        f.write('------\n')
+        for k in perc:
+            f.write(f'Average {k} improve %: {perc[k]/total:.5f}%\n')
+        f.write('======\n')
